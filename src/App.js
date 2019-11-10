ab5e0d73b97e2591da479694571ebe3bc6bb989e
Kensuke Shibata <kensukeshibata@gmail.com>
2019-11-10 17:24:01 +0900

    setState input Search field

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
-  state = { fighters: [] };
+  state = {
+    fighters: [],
+    searchField: ""
+  };
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -14,6 +17,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search fighters"
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList fighters={this.state.fighters}></CardList>
       </div>
     );
