a748ab5fb5fd6bbb29ffcebb65ad51944f221171
Kensuke Shibata <kensukeshibata@gmail.com>
2020-03-31 00:55:29 +0900

    convet to Hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    fighters: [],
-    searchField: ""
-  };
+const App = () =>  {
+  const [fighters, setFighters] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ fighters: users }));
-  }
+      .then(users => setFighters({ fighters: users }));
+  }, [])
 
-  handleChange = e => {
-    this.setState({ searchField: e.target.value });
+
+
+  const handleChange = e => {
+    setSearchField({ searchField: e.target.value });
   };
 
-  render() {
-    const { fighters, searchField } = this.state;
     const filterdFighters = fighters.filter(fighter =>
       fighter.name.toLowerCase().includes(searchField.toLowerCase())
     );
+
     return (
       <div className="App">
         <h1>Doragon Ball Fighters</h1>
         <SearchBox
           placeholder="search fighters"
-          handleChange={this.handleChange}
+          handleChange={handleChange}
         />
 
         <CardList fighters={filterdFighters}></CardList>
       </div>
     );
-  }
 }
-
 export default App;
